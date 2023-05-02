dedc3e04a05a2f17f9134d28db52a9c802895021
Douglas Fils <drfils@gmail.com>
2023-05-02 12:53:38 -0500

    parquet to RDF work

---
+++ code/parquetToRDF/README.md
@@ -0,0 +1,13 @@
+# Parquet To RDF
+
+## NOTE
+
+this section is only a DRAFT..   the code here is very early stages and needs a lot of attention.
+
+## About
+
+A simple program to read a parquet file from the csvToParquet code and convert
+this to RDF.  The schema follows the FDO pattern and is implemented in schame.org.
+
+Reference:  https://github.com/nsdf-fabric/nsdf-catalog-graph/blob/master/docs/work.md#fair-digital-object
+
---
+++ code/parquetToRDF/parquetToRDF.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import pyarrow as pa
+import os
+import pyarrow.parquet as pq
+import json
+from pyld import jsonld
+from pyld import documentloader
+
+
+# Simple program to read a parquet file and convert it to schema.org
+# following FROF patterns and profiles
+def myloader(*args, **kwargs):
+    requests_loader = documentloader.requests.requests_document_loader(*args, **kwargs)
+
+    def loader(url, options={}):
+        options['headers']['Accept'] = 'application/ld+json'
+        return requests_loader(url, options)
+
+    return loader
+
+
+def generate_json_ld(repository, collection, name, size, lastmodified, etag):
+    # Create a dictionary representing the JSON-LD object
+
+    context = {
+        "@vocab": "http://schema.org/",
+        "name": "name",
+        "description": "description",
+        "url": "url"
+    }
+
+    json_ld = {
+        "@context": {"@vocab": "https://schema.org/"},
+        "@type": "DigitalDocument",
+        # "@id": "https://example.org/datasets/1234567890",
+        "name": name,
+        "producer": repository,
+        "isPartOf": collection,
+        "publishingPrinciples": [
+            {
+                "@type": "CreativeWork",
+                "@id": "https://sdgs.un.org/goals/goal14",
+                "url": "https://sdgs.un.org/goals/goal14",
+                "name": "Kernel Information Profile",
+                "description": " description"
+            },
+            {
+                "@type": "CreativeWork",
+                "@id": "https://dx.doi.org/10.1038/sdata.2016.18",
+                "url": "https://www.nature.com/articles/sdata201618",
+                "name": "Digital Obkect Policy",
+                "description": " description"
+            }
+        ],
+        "encoding": {
+            "@type": "MediaObject",
+            "sha256": etag,
+            "contentSize": size
+        },
+        "distribution": "https://example.org/datasets/1234567890",
+        "dateModified": lastmodified
+    }
+
+    # jsonld.set_document_loader(myloader())
+
+    # compacted = jsonld.compact(json_ld, context)
+    json_ld_str = json.dumps(json_ld, indent=2)
+
+    return json_ld_str
+
+
+new_schema = pa.schema([
+    ('repository', pa.string()),
+    ('collection', pa.string()),
+    ('name', pa.string()),
+    ('size', pa.int64()),
+    ('lastmodified', pa.string()),
+    ('etag', pa.string())
+])
+
+csv_column_list = ['repository', 'collection', 'name', 'size', 'lastmodified', 'etag']
+file_path = "/mnt/wdb/Data/NSDF/parquet/mdf/w_14_v1.1.parquet"
+
+# Read the Parquet file using pyarrow
+table = pq.read_table(file_path)
+
+# Convert the table to a Pandas DataFrame
+df = table.to_pandas()
+
+# Filter the DataFrame to keep only the specified columns
+df = df[csv_column_list]
+
+# Iterate through each row and print the values
+for index, row in df.iterrows():
+    # print(row['repository'], row['collection'], row['name'], row['size'], row['lastmodified'], row['etag'])
+    fn = "/mnt/wdb/Data/NSDF/graphs/{}{}.json".format(row['repository'], index)
+    # print(fn)
+    json_ld_output = generate_json_ld(row['repository'], row['collection'], row['name'], row['size'], row['lastmodified'], row['etag'])
+    with open(fn, 'w') as file:
+        file.write(json_ld_output)
+    
+    # print(json_ld_output)
+
+# # Example usage
+# repository = "The repository"
+# collection = "The collection"
+# name = "DO name"
+# size = "DO size"
+# lastmodified = "Last mod"
+# etag = "etag value"
+# 
+# json_ld_output = generate_json_ld(repository, collection, name, size, lastmodified, etag)
+# print(json_ld_output)
